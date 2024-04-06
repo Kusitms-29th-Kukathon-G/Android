bedf2836dc3d104c44aa285baf4346a77032e3f0
gitsuhyun <suhyun10201@naver.com>
2024-04-06 17:17:40 +0900

    [Feat] splash 추가

--- app/src/main/java/com/example/kukathon/MainActivity.kt
+++ app/src/main/java/com/example/kukathon/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kukathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kukathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        (R.style.Theme_Kukathon)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
